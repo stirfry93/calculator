a20fa95e700b8c5d7e2147b3cb1abf1d61c9e1fd
Sterling <sterlingmrichards@gmail.com>
2023-05-03 14:53:49 -0500

    Fix some bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const operate = function(num1, operator, num2) {
 
-    let output = 0;
+    let output;
 
     switch (operator) {
         case '+':
@@ -15,9 +15,35 @@ const operate = function(num1, operator, num2) {
         case '÷':
             output = num1 / num2;
             break;
+        default:
+            output = num2;
     };
 
     return output;
+
+};
+
+const stringedOperate = function(stringedNum1, stringedOperator, stringedNum2) {
+
+    switch (stringedOperator) {
+        case '+':
+            newNum1 = stringedNum1 + stringedNum2;
+            break;
+        case '−':
+            newNum1 = stringedNum1 - stringedNum2;
+            break;
+        case '×':
+            newNum1 = stringedNum1 * stringedNum2;
+            break;
+        case '÷':
+            newNum1 = stringedNum1 / stringedNum2;
+            break;
+        default:
+            newNum1 = stringedNum2;
+    };
+
+    return newNum1;
+    
 
 };
 
@@ -25,7 +51,7 @@ const numbers = document.querySelectorAll(".number");
 
 const displayValue = [];
 
-const globalScopeVar = {}; // global-scoped variable to hold num1 and operator
+const globalScopeVar = {}; // global-scoped variable to hold num1, operator, and num2. my question is, though, should i not be holding these values in both this object AND the array displayValue? is that redundant?
 
 numbers.forEach((item) => {
     item.addEventListener('click', (Event) => {
@@ -37,57 +63,88 @@ numbers.forEach((item) => {
 
 const operators = document.querySelectorAll(".operator");
 
-const displayText = document.getElementById("displayText");
-
 operators.forEach((item) => {
     item.addEventListener('click', (Event) => {
-
-    let num1 = displayText.textContent;
-
-    displayValue.push(Event.target.textContent); 
-    displayText.textContent = displayValue.join('');
-    globalScopeVar.firstNumber = num1;
-    // if operator is pressed more than once, ignore 
-
-    let operator = Event.target.textContent;
-    globalScopeVar.operatorChoice = operator;
     
+        let num1 = displayText.textContent;
+
+        displayValue.push(Event.target.textContent); 
+        displayText.textContent = displayValue.join('');
+        globalScopeVar.firstNumber = Number(num1);
+    
+        let operator = Event.target.textContent;
+        globalScopeVar.operatorChoice = operator;
+
+        console.log(" this is num1 " + num1);
+        // console.log(" this is operator " + operator);
+
+        if (num1.indexOf(operator) > -1) {
+            let stringedNum1 = Number(num1.substring(0, num1.indexOf(operator)));
+            globalScopeVar.firstStringedNumber = stringedNum1;
+            console.log('this is stringed1 ' + stringedNum1);
+            console.log(globalScopeVar.firstStringedNumber);
+            let regex = /[+|−|×|÷]/; //only working with + currently
+            let stringedOperator = num1.match(regex)[0];
+            console.log(num1.match(regex)[0] + " this is regex match");
+            globalScopeVar.stringedOperatorChoice = stringedOperator;
+            console.log('this is stringedOperator ' + stringedOperator);
+            let stringedNum2 = Number(num1.split(regex)[1]); // this is only taking the last digit, not the entire number after the regex
+            globalScopeVar.secondStringedNumber = stringedNum2;
+            console.log('this is stringed2 ' + stringedNum2);
+            console.log(stringedOperate(stringedNum1, stringedOperator, stringedNum2));
+        };
+        
+        // when an operator is clicked, check whether i've already clicked an operator before
+
     });
+
 });
+
 
 const equals = document.getElementById("equals");
 
 equals.addEventListener('click', (Event) => {
-    let num1 = Number(globalScopeVar.firstNumber);
-    let operator = globalScopeVar.operatorChoice;
-
-    let operatorPlace = displayValue.indexOf(operator);
-    let num2 = Number(displayValue.slice(operatorPlace + 1).join(''));
     
-    console.log(num1 + " (num1)");
-    console.log(operator + " (operator)");
-    console.log(num2 + " (num2)");
-    console.log(displayValue);
+    if (globalScopeVar.firstStringedNumber) { 
+        let num1 = newNum1;
+        let operator = globalScopeVar.operatorChoice;
+        let num2 = Number(displayText.textContent.charAt(displayText.textContent.length - 1));
+        operate(num1, operator, num2);
+        console.log(operate(num1, operator, num2) + " (operate function)");
 
-    operate(num1, operator, num2);
+        displayText.textContent = Math.round(100 * operate(num1, operator, num2))/100;
+    
+        if (operator === '÷' && num2 === 0) {
+            displayText.textContent = "undefined";
+        };
+    } else {
+        let num1 = Number(globalScopeVar.firstNumber);
+        console.log(num1);
+        let operator = globalScopeVar.operatorChoice;
+        let operatorPlace = displayValue.indexOf(operator);
+        let num2 = Number(displayValue.slice(operatorPlace + 1).join(''));
+        globalScopeVar.secondNumber = num2;
+        operate(num1, operator, num2);
+        console.log(operate(num1, operator, num2) + " (operate function)");
 
-    console.log(operate(num1, operator, num2) + " (operate function)");
+        displayText.textContent = Math.round(100 * operate(num1, operator, num2))/100;
+    
+        if (operator === '÷' && num2 === 0) {
+            displayText.textContent = "undefined";
+        };
+    };
+    
 
-    displayText.textContent = Math.round(100 * operate(num1, operator, num2))/100;
-
-    if (operator === '÷' && num2 === 0) {
-        displayText.textContent = "undefined";
-    }
 });
 
 const clear = document.getElementById("clear");
 
 clear.addEventListener('click', (Event) => {
     displayValue.length = 0;
-    let globalScopeVar = {};
     displayText.textContent = "0";
 });
 
-// how to string together multiple operators??
-// when = is clicked before num2 has been entered, return num1 (this will go in the equals event- if num2 == undefined, return num1)
-// make sure displayText does not overflow screen
+console.log(globalScopeVar);
+
+
+// i need to make sure AC is completely clearing all data, because I noticed weird things are happening when i have solved multiple equations on the calculator
